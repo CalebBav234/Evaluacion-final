9867d3e2bf41e61a893add33bfaaebd4c79a2c2a
CalebBav234 <calebmbaving@gmail.com>
2025-12-19 17:10:44 -0400

    Feat: created ResetPasswordDto

---
+++ Entities/Dtos/Auth/ResetPassword.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoFinalTecWeb.Entities.Dtos.Auth
+{
+    public class ResetPassword
+    {
+        public string token { get; set; } = string.Empty;
+        public string newPassword { get; set; } = string.Empty;
+    }
+}
